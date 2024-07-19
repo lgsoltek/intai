2c5ebbb18a5fb1ed8591be5b28cb58bf0396d6fd
lgsoltek <lgsoltek@gmail.com>
2024-07-19 16:09:28 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -155,10 +155,10 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          NextChat
+          TransAssist
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          Pour améliorer votre traduction.
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
